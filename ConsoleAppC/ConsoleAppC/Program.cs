143d030eb268c11e0367f755328b9e1f16ba8ccf
dweinmann <dontspamme@gmx.org>
2020-02-24 12:37:17 +0100

    Add try-cach block

--- ConsoleAppC/ConsoleAppC/Program.cs
+++ ConsoleAppC/ConsoleAppC/Program.cs
@@ -24,58 +24,82 @@ namespace ConsoleAppC
             {
                 Console.Write("Datei lesen (l) oder schreiben (s), beenden (x): ");
                 op = Console.ReadLine();
-
-                switch (op)
+                try
                 {
-                    case "l":
-                        //Streamreader oeffnen und Datei einlesen
-                        sr = new StreamReader(@datei);
+                    switch (op)
+                    {
+                        case "l":
+                            //Streamreader oeffnen und Datei einlesen
+                            sr = new StreamReader(@datei);
 
-                        //Erste Zeile aus Datei einlesen
-                        zeile = sr.ReadLine();
-                        while (zeile != null)
-                        {
-                            inhalt = zeile.Split(';');
-                            name = inhalt[0];
-                            alter = Convert.ToInt32(inhalt[1]);
-                            Console.WriteLine("{0} ist {1} Jahre alt.", name, alter);
-                            //Naechste Zeile einlesen
+                            //Erste Zeile aus Datei einlesen
                             zeile = sr.ReadLine();
-                        }
-                        sr.Close();
-                        sr = null;
-                        break;
-
-                    case "s":
-                        {
-                            //StreamWriter oeffnen und Datei erzeugen
-                            sw = new StreamWriter(@datei);
-
-                            Console.WriteLine("Daten einlesen (beenden mit Name = -1):");
-                            while (true)
+                            while (zeile != null)
                             {
-                                Console.Write("Eingabe Alter: ");
-                                alter = Convert.ToInt32(Console.ReadLine());
-
-                                if (alter == -1)
-                                    break;
-
-                                Console.Write("Eingabe Name : ");
-                                name = Console.ReadLine();
-
-                                sw.WriteLine(name + ";" + alter.ToString());
+                                inhalt = zeile.Split(';');
+                                name = inhalt[0];
+                                alter = Convert.ToInt32(inhalt[1]);
+                                Console.WriteLine("{0} ist {1} Jahre alt.", name, alter);
+                                //Naechste Zeile einlesen
+                                zeile = sr.ReadLine();
                             }
-                            sw.Close();
-                            sw = null;
-                        }
-                        break;
+                            break;
 
-                    case "x":
-                        break;
+                        case "s":
+                            {
+                                //StreamWriter oeffnen und Datei erzeugen
+                                sw = new StreamWriter(@datei);
 
-                    default:
-                        Console.WriteLine("Operation '{0}' nicht definiert!", op);
-                        break;
+                                Console.WriteLine("Daten einlesen (beenden mit Name = -1):");
+                                while (true)
+                                {
+                                    Console.Write("Eingabe Alter: ");
+                                    alter = Convert.ToInt32(Console.ReadLine());
+
+                                    if (alter == -1)
+                                        break;
+
+                                    Console.Write("Eingabe Name : ");
+                                    name = Console.ReadLine();
+
+                                    sw.WriteLine(name + ";" + alter.ToString());
+                                }
+                            }
+                            break;
+
+                        case "x":
+                            break;
+
+                        default:
+                            Console.WriteLine("Operation '{0}' nicht definiert!", op);
+                            break;
+                    }
+                }
+                catch (FileNotFoundException fnfe)
+                {
+                    Console.WriteLine("Fehler! Datei nicht gefunden.");
+                }
+                catch(DirectoryNotFoundException dnfe)
+                {
+                    Console.WriteLine("Fehler! Verzeichnis im Pfad existiert nicht.");
+                }
+                catch (UnauthorizedAccessException uae)
+                {
+                    Console.WriteLine("Fehler! Kein Zugriff auf den angegebenen Pfad.");
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine("Fehler! Eingabe ist keine Zahl!");
+                }
+                finally
+                {
+                    // Streamreader schließen
+                    sr?.Close();
+                    sr = null;
+
+                    // StreamWriter schließen
+                    sw?.Close();
+                    sw = null;
                 }
             } while (op != "x");
         }
